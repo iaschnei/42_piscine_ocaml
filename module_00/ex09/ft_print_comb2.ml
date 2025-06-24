420fa162f40270ded7b7e5921a0658ccf5c46501
vboxuser <iaschnei@student.42.fr>
2025-06-24 13:23:41 +0000

    added mod00 ex09

--- module_00/ex09/ft_print_comb2.ml
+++ module_00/ex09/ft_print_comb2.ml
@@ -1,1 +1,29 @@
+let print_two_digits n = 
+  let tens = n / 10 in
+  let units = n mod 10 in 
+  print_int(tens);
+  print_int(units)
+
 let ft_print_comb2 () = 
+  let rec loop a b = 
+    if a < 99 then
+      begin
+        print_two_digits a;
+        print_char ' ';
+        print_two_digits b;
+        if not (a = 98 && b = 99) then
+          begin
+            print_char ',';
+            print_char ' '
+          end;
+        if b < 99 then
+          loop a (b + 1)
+        else
+          loop (a + 1) (a + 2)
+      end
+    else
+      print_char '\n'
+    in
+  loop 0 1
+
+
